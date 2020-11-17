35d4b35643aa121aef4971a9f59cc931f03bd448
Crowdbotics <team@crowdbotics.com>
2020-11-17 22:10:46 +0000

    Update models

--- backend/funes_landscaping_22708/wsgi.py
+++ backend/funes_landscaping_22708/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'funes_landscaping_22708.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "funes_landscaping_22708.settings")
 
 application = get_wsgi_application()
